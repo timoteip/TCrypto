bb99333fd1e52b2d4986ae9898dc6168fd582bf5
Timotei Perju <timotei.perju@emanuel.ro>
2021-06-08 19:17:43 +0300

    Rankings

---
+++ src/components/UserRankingItem/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import styles from './style';
+// import image from '';
+
+const image =  require('../../../assets/images/Saly-10.png');
+
+export interface UserRankingItemProps {
+    user: {
+        image: string,
+        name: string,
+        netWorth: number
+    },
+    place: number,   
+}
+
+const UserRankingItem = (props: UserRankingItemProps) => {
+    const {
+        user: {
+            image,
+            name,
+            netWorth
+        },
+        place
+
+    } = props;
+    return (
+        <View style={styles.root}>
+            <View style={styles.left}>
+                <Text style={styles.place}>{place}</Text>
+                <Image style={styles.image} source={{ uri: image}} />
+                <View>
+                    <Text style={styles.name}>{name}</Text>
+                </View>
+            </View>
+            <View style={{alignItems: 'flex-end'}}>
+                <Text style={styles.name}>€{netWorth}</Text>
+            </View>
+        </View>
+    );
+};
+
+export default UserRankingItem;
--- src/screens/RankingsScreen/index.tsx
+++ src/screens/RankingsScreen/index.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, FlatList} from 'react-native';
+import styles from './styles';
+import UserRangeItem from '../../components/UserRankingItem';
 
-const RankingsScreen = (props) => {
+const image =  require('../../../assets/images/Saly-20.png');
+
+const portofolioCoins = [{
+    id: '1',
+    name: 'Virtual EURO',
+    image: 'https://image.freepik.com/free-vector/cryptocurrency-logo-template_8163-114.jpg',
+    netWorth: 100000,
+}, {
+    id: '2',
+    name: 'Bitcoin',
+    image: 'https://image.freepik.com/free-vector/cryptocurrency-logo-template_8163-114.jpg',
+    netWorth: 75994,
+}, {
+    id: '4',
+    name: 'Etherium',
+    image: 'https://image.freepik.com/free-vector/cryptocurrency-logo-template_8163-114.jpg',
+    netWorth: 80153,
+}]
+
+const PortofolioScreen = () => {
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>RankingsScreen Component</Text>
+        <View style={styles.root}>
+            <FlatList 
+                showsVerticalScrollIndicator={false}
+                style={{width: '100%'}}
+                data={portofolioCoins}
+                renderItem={({item, index}) => <UserRangeItem user={item} place={index + 1} />}
+                ListHeaderComponentStyle={{alignItems: 'center'}}
+                ListHeaderComponent={() => (
+                    <>
+                        <Image style={styles.image} source={image} />
+                        <Text style={styles.label}>Rankings</Text>
+                    </>
+                )}
+            />
         </View>
     );
 };
 
-export default RankingsScreen;
+export default PortofolioScreen;
