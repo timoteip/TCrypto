e7e0cf7bb86b29e22b34df10507126db95d43736
Timotei Perju <timotei.perju@emanuel.ro>
2021-06-09 20:43:26 +0300

    welcome screen ui

---
+++ src/screens/WelcomeScreen/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, Image, Pressable} from 'react-native';
+import styles from './styles';
+const image =  require('../../../assets/images/Saly-1.png');
+const googleButtonImage =  require('../../../assets/images/google-login.png');
+
+const WelcomeScreen = () => {
+  const signInGoogle = () => {
+
+  }
+  return (
+    <View style={styles.root}>
+      <Image style={styles.image} source={image} />
+      <Text style={styles.header1}>Welcome to VCrypto</Text>
+      <Text style={styles.header2}>Invest your virtual $100.000 and compete with others</Text>
+
+      <Pressable onPress={signInGoogle} style={styles.googleButton}>
+        <Image style={styles.buttonImage} source={googleButtonImage}/>
+      </Pressable>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
